90d35ad35a7ee3dd9845a19bc5f39a4f926214a5
felipe gatica <felipe.gatica.ramirez@gmail.com>
2021-11-09 03:42:22 -0300

    Add files via upload
    Paso de variables entre pantallas

---
+++ components/ContactList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {Link} from 'react-router-dom';
+import ContactCard from "./ContactCard";
+ 
+const ContactList = (props) => {
+	console.log(props);
+	const deleteContactHandler = (id) => {
+		props.getContactId(id);
+	};
+	
+	
+	const renderContacList = props.contacts.map((contact) => {
+		return 	<ContactCard 
+				contact={contact} 
+				clickHander = {deleteContactHandler}
+				key={contact.id}/>;
+	});
+	return( 
+		<div className="main">
+			<h2>Lista de Usuari@s</h2>
+			
+			<div>
+			<Link to="/agregar">
+				<button className="ui button blue right">
+					Agregar Usuari@
+				</button>
+			</Link>
+			</div>
+		
+		<div className="ui celled list">
+		{renderContacList}
+		</div>
+		</div>
+		);
+};
+	
+export default ContactList;

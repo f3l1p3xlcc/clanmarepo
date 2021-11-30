065d1c9edd0698237c6c6771855ba858a4e84a42
felipe gatica <felipe.gatica.ramirez@gmail.com>
2021-11-30 09:35:28 -0300

    Add files via upload
    se corrige el paso de variables entre las pantallas.

---
+++ src/components/ContactCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import user from "../images/person.png";
+
+import xmark from "../images/xmark.circle.png";
+
+const ContactCard =(props) => {
+	const {id,nombre,email} = props.contact
+	return(
+			<div className="item">
+			<img className="ui avatar image" src={user} alt="user" />
+				<div className="content">
+				<Link to={{pathname: '/contact/${id}', state:{contact:props.contact}}}>
+				
+					<div className="header">{email}</div>
+				</Link>
+				
+				</div>
+				
+				<img className="ui avatar image" src={xmark} alt="user" onClick={() => props.clickHander(id)} />
+				
+			</div>	
+);
+};
+export default ContactCard;
